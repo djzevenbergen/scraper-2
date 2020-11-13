2ffe6dc70273e7b0445dfc989f4a8b04f8a8758b
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-11-13 12:43:27 -0800

    add functionality for scraping for prices

--- parse-urls.py
+++ parse-urls.py
@@ -4,28 +4,30 @@ import csv
 if __name__ == "__main__":
 
     import csv
-    with open('appliances-Kismile-Removable-Standing-Adjustable-Temperature', newline='') as f:
+    with open('appliances.csv', newline='') as f:
         reader = csv.reader(f)
         f = open(f"appliances.txt", "a")
         chunks = ''
+        otherCount = 1
         for row in reader:
             if 'product-title' in row[1]:
                 count = 0
-                f.write(row[1] + "\n")
+                f.write(str(otherCount) + "-- " + row[1] + "\n")
                 print(row[1])
+                otherCount += 1
             elif 'https://images-na.ssl-images-amazon.com/images/I/' in row[1]:
-
+                s = "."
                 if count == 0:
                     chunks = row[1].split('.')
                     temp = chunks[-2:]
-                    f.write(row[1] + "\n")
+                    f.write(s.join(chunks[0:-2]) + '.jpg' + "\n")
                     print(row[1])
                     count += 1
                 else:
                     chunks = row[1].split('.')
                     print(chunks)
                     if chunks[-2:] == temp:
-                        f.write(row[1] + "\n")
+                        f.write(s.join(chunks[0:-2]) + '.jpg' + "\n")
                         print(row[1])
 
         f.close()
--- read-file.py
+++ read-file.py
@@ -2,35 +2,43 @@ from lxml.html import fromstring
 import requests
 from itertools import cycle
 import traceback
+import urllib3
 
 
 def get_proxies():
-    url = 'https://free-proxy-list.net/'
+    url = 'https://hidemy.name/api/proxylist.txt?country=US&maxtime=1700&type=hs&out=plain&lang=en&utf&code=709444465162232'
     response = requests.get(url)
-    parser = fromstring(response.text)
-    proxies = set()
-    for i in parser.xpath('//tbody/tr')[:10]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
-            proxy = ":".join([i.xpath('.//td[1]/text()')[0],
-                              i.xpath('.//td[2]/text()')[0]])
-            proxies.add(proxy)
-    return proxies
+    parser = response.text
+    print(parser)
+    # proxies = set()
+    # for i in parser:
+    #     proxy = i
+    #     print(i)
+    #     proxies.add(proxy)
+    # return proxies
+    return parser
 
 
-# If you are copy pasting proxy ips, put in the list below
-#proxies = ['121.129.127.209:80', '124.41.215.238:45169', '185.93.3.123:8080', '194.182.64.67:3128', '106.0.38.174:8080', '163.172.175.210:3128', '13.92.196.150:8080']
-proxies = get_proxies()
-proxy_pool = cycle(proxies)
+if __name__ == "__main__":
+    thing = get_proxies()
+    proxies = thing.split()
+    print(proxies)
+    # If you are copy pasting proxy ips, put in the list below
+    # proxies = ['121.129.127.209:80', '124.41.215.238:45169', '185.93.3.123:8080', '194.182.64.67:3128', '106.0.38.174:8080', '163.172.175.210:3128', '13.92.196.150:8080']
+    # proxies = get_proxies()
 
-url = 'https://httpbin.org/ip'
-for i in range(1, 11):
-    # Get a proxy from the pool
-    proxy = next(proxy_pool)
-    print("Request #%d" % i)
-    try:
-        response = requests.get(url, proxies={"http": proxy, "https": proxy})
-        print(response.json())
-    except:
-        # Most free proxies will often get connection errors. You will have retry the entire request using another proxy to work.
-        # We will just skip retries as its beyond the scope of this tutorial and we are only downloading a single url
-        print("Skipping. Connnection error")
+    proxy_pool = cycle(proxies)
+
+    url = 'https://httpbin.org/ip'
+    for i in range(1, len(proxies) - 1):
+        # Get a proxy from the pool
+        proxy = next(proxy_pool)
+        print("Request #%d" % i)
+        try:
+            response = requests.get(
+                url, proxies={"https": "http://" + proxy, "http": "http://" + proxy})
+            print(response.json())
+        except:
+            # Most free proxies will often get connection errors. You will have retry the entire request using another proxy to work.
+            # We will just skip retries as its beyond the scope of this tutorial and we are only downloading a single url
+            print("Skipping. Connnection error")
